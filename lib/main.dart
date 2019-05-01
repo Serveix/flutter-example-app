d4327e4b7cee858b739b58036078b6e544650b5e
Eli <eli@cheffy.mx>
2019-04-30 20:43:36 -0500

    Pass info to another screen in a list

--- lib/main.dart
+++ lib/main.dart
@@ -3,46 +3,69 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: 'Navigation Basics',
-    home: FirstRoute(),
+    home: FirstRoute(
+      todos: List.generate(
+        20, 
+        (i) => Element(
+          "Element $i",
+          "Description item $i",
+        ),
+      ),
+    ),
   ));
 }
 
+class Element {
+  final String title;
+  final String description;
+
+  Element(this.title, this.description);
+}
+
 class FirstRoute extends StatelessWidget {
+  final List<Element> todos;
+
+  FirstRoute({Key key, @required this.todos}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('First Route'),
       ),
-      body: Center(
-        child: RaisedButton(
-          child: Text('Open route'),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => SecondRoute()),
-            );
-          },
-        ),
+      body: ListView.builder(
+        itemCount: todos.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(todos[index].title),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SecondRoute(todo: todos[index]),
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
 }
 
 class SecondRoute extends StatelessWidget {
+  final Element todo;
+  SecondRoute({Key key, @required this.todo}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Second Route"),
+        title: Text(todo.title),
       ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Go back!'),
-        ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Text(todo.description),
       ),
     );
   }
